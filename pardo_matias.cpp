38ca6261d6033c4ae7b71fe35a413a06eded815d
pardomatias <pardoserrudo.matiasnahuel.studente@itispaleocapa.it>
2026-02-20 12:00:38 +0100

    Update pardo_matias.cpp

--- pardo_matias.cpp
+++ pardo_matias.cpp
@@ -9,14 +9,20 @@ using namespace std;
 		string categoria;
 	};
 
-void aggiungi(prodotto y,prodotto x[],int d)
+void aggiungi(prodotto y,prodotto x[],int &d)
 {
-	
+	x[d]=y;
+	d++;
 }
 
 void visualizza(prodotto x[],int d)
 {
-	
+	for(int j = 0; j < d; j++)
+	{
+		cout<<"\n il nome del prodotto e': "<<x[j].nome;
+	    cout<<"\n la categoria e': "<<x[j].categoria;
+        cout<<"\n il prezzo e': "<<x[j].prezzo;
+	}
 }
 
 void cerca(prodotto y,prodotto x[],int d)
@@ -37,6 +43,54 @@ void modifica(prodotto y,prodotto x[],int d)
 int main(int argc, char** argv) {
 	
 	prodotto supermercato[100];
+    int v, i = 0;
+	do
+	{
+		cout<<"\n1: aggiungi \n";
+		cout<<"2: visualizza \n";
+		cout<<"3: cerca \n";
+		cout<<"4: cancella \n";
+		cout<<"5: modifica \n";
+		cout<<"0: finire il programma \n";
+		cin>>v;
+		
+		switch(v)
+		{
+			case 1:
+				cout<<"\n inserisci il nome del prodotto: ";
+	            cin>>supermercato[i].nome;
+	            cout<<"\n inserisci la categoria: ";
+	            cin>>supermercato[i].categoria;
+                cout<<"\n inserisci il prezzo: ";
+                cin>>supermercato[i].prezzo;
+				aggiungi(supermercato[i],supermercato,i);
+				break;
+				
+			case 2:
+			 	
+			    visualizza(supermercato,i);
+			    break;
+			    
+			case 3:
+			    cout<<"cosa vuoi cercare?\n";
+			    cin>>
+			   // cerca();
+			    break;
+			    
+			case 4:
+			 	
+			 //	cancella();
+			    break;
+			    
+			case 5:
+			 	
+			 //	modifica();
+			    break;
+			    
+		}
+		
+		
+	}while(v!=0);
 	
 
 	
