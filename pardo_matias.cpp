df7e0bff6a8a42d8a4f8f498bdcdbe8db6023fe7
pardomatias <pardoserrudo.matiasnahuel.studente@itispaleocapa.it>
2026-02-20 10:32:10 +0100

    Update pardo_matias.cpp

--- pardo_matias.cpp
+++ pardo_matias.cpp
@@ -2,14 +2,39 @@
 #include <string>
 using namespace std;
 
-
-int main(int argc, char** argv) {
 	struct prodotto
 	{
 		string nome;
 		float prezzo;
 		string categoria;
 	};
+
+void aggiungi(prodotto y,prodotto x[],int d)
+{
+	
+}
+
+void visualizza(prodotto x[],int d)
+{
+	
+}
+
+void cerca(prodotto y,prodotto x[],int d)
+{
+	
+}
+
+bool cancella(prodotto y,prodotto x[],int d)
+{
+	
+}
+
+void modifica(prodotto y,prodotto x[],int d)
+{
+	
+}
+
+int main(int argc, char** argv) {
 	
 	prodotto supermercato[100];
 	
